f6a5ba36aa2697c34b114fa67934c7ac10e3c0a7
Siarhei_Wehrhahn <sergei1997.sw@gmail.com>
2024-12-08 22:53:06 +0100

    bis references responsive gemacht

--- portfolio/src/app/main-view/references/references.component.ts
+++ portfolio/src/app/main-view/references/references.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-references',
@@ -9,9 +9,25 @@ import { Component } from '@angular/core';
   styleUrl: './references.component.scss'
 })
 export class ReferencesComponent {
+  @ViewChild('references') referencesContainer!: ElementRef;
+
   references = [
     {name: 'Sebastian Böhme', project: 'Project Kochwelt', reference: 'Während des Gruppenprojekts "Kochwelt" am Anfang des Kurses hat Siarhei immer wieder gezeigt wie hilfsbereit und zuvorkommend er ist. Durch seine fortgeschrittene Erfahrung, konnte er den Mitgliedern unserer Gruppe immer wieder wertvolle Tipps geben und hat damit beachtlich zum Erfolg des Projekts beigetragen. Auch außerhalb von Gruppenprojekten, stand er seinen Mitmenschen immer mit Rat und Tat zur Seite. Ein echter Teamplayer.'},
     {name: 'Jason', project: 'Project Join', reference: 'I had the pleasure of working with Siarhei on the group project JOIN. He was always reliable and demonstrated impressive quick learning skills, as well as the ability to effectively apply and deepen new knowledge. Thanks to his excellent communication skills, Siarhei managed the group confidently and ensured productive collaboration. His helpfulness and dedication made working with him a truly enjoyable experience. I highly recommend Siarhei for future projects.'},
     {name: 'Ben Dei', project: 'Projecthilfe', reference: "Although I didn't have the chance to work closely with Siarhei, he still left a lasting impression. His willingness to help and his extensive knowledge proved invaluable in solving some of my challenges. He always took the time to understand my issues and provided thoughtful, solution-oriented support. Collaborating with him would undoubtedly be a great asset!"},
   ]
+
+  currentReferenceIndex = 0;
+
+  scrollToReference(index: number) {
+    const container = this.referencesContainer.nativeElement;
+    const referenceWidth = container.offsetWidth;
+
+    container.scrollTo({
+      left: index * referenceWidth,
+      behavior: 'smooth',
+    });
+
+    this.currentReferenceIndex = index;
+  }
 }
