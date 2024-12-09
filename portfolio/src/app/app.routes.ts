3f669d901a1b2b4d41d3269cf32681eed2d4309b
Siarhei_Wehrhahn <sergei1997.sw@gmail.com>
2024-12-09 19:45:44 +0100

    datenschutzerklärung design fertig jedoch mit kleinem fehler im kontaktformular

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { MainViewComponent } from './main-view/main-view.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
     {path: '', component: MainViewComponent}, 
+    {path: 'privacy-policy', component: PrivacyPolicyComponent}, 
 ];
