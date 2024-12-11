56744f752bfb7b57f51c2199b842d89524e6de47
Siarhei_Wehrhahn <sergei1997.sw@gmail.com>
2024-12-11 16:48:18 +0100

    Animation klappt herforagend

--- portfolio/src/app/main-view/titel-view/titel-view.component.ts
+++ portfolio/src/app/main-view/titel-view/titel-view.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-titel-view',
@@ -7,6 +7,14 @@ import { Component } from '@angular/core';
   templateUrl: './titel-view.component.html',
   styleUrl: './titel-view.component.scss'
 })
-export class TitelViewComponent {
+export class TitelViewComponent implements AfterViewInit {
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
+  ngAfterViewInit(): void {
+    const h1Element = this.el.nativeElement.querySelector('.name');
+    const h2Element = this.el.nativeElement.querySelector('.job');
+
+    this.renderer.addClass(h1Element, 'animate');
+    this.renderer.addClass(h2Element, 'animate');
+  }
 }
