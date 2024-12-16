7ed01356882923ea34df79344e8fe6b92f323ee2
Siarhei_Wehrhahn <sergei1997.sw@gmail.com>
2024-12-16 20:24:18 +0100

    Overlay fertig funktion fehlt nur

--- portfolio/src/app/main-view/titel-view/titel-view.component.ts
+++ portfolio/src/app/main-view/titel-view/titel-view.component.ts
@@ -9,15 +9,26 @@ import { AfterViewInit, Component, ElementRef, Renderer2 } from '@angular/core';
 })
 export class TitelViewComponent implements AfterViewInit {
   constructor(private el: ElementRef, private renderer: Renderer2) {}
+  isOverlayVisible = false;
 
   ngAfterViewInit(): void {
     const h1Element = this.el.nativeElement.querySelector('.name');
     const h22Element = this.el.nativeElement.querySelector('.sureName');
     const h2Element = this.el.nativeElement.querySelector('.job');
 
-    // Set classes with delays for animations
-    setTimeout(() => this.renderer.addClass(h1Element, 'animate'), 0); // Immediate
-    setTimeout(() => this.renderer.addClass(h22Element, 'animate'), 500); // After 0.5s
-    setTimeout(() => this.renderer.addClass(h2Element, 'animate'), 1000); // After 1s
+    setTimeout(() => this.renderer.addClass(h1Element, 'animate'), 0);
+    setTimeout(() => this.renderer.addClass(h22Element, 'animate'), 500);
+    setTimeout(() => this.renderer.addClass(h2Element, 'animate'), 1000);
+  }
+
+  toggleOverlay() {
+    this.isOverlayVisible = !this.isOverlayVisible;
+
+    const overlayElement = this.el.nativeElement.querySelector('.overlayMenue');
+    if (this.isOverlayVisible) {
+      this.renderer.setStyle(overlayElement, 'display', 'flex');
+    } else {
+      this.renderer.setStyle(overlayElement, 'display', 'none');
+    }
   }
 }
